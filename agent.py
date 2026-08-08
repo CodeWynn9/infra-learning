8033d8e5618d5c4f0dd4cb473530f6f64220df66
wind <wind@winddeMacBook-Pro.local>
2026-08-08 18:31:03 +0800

    restore projetct to 891c831 and commit all

---
+++ agent.py
@@ -0,0 +1,9 @@
+print("agent.py 中的 __name__ 是：", __name__)
+
+
+class Agent:
+    pass
+
+
+if __name__ == "__main__":
+    print("agent.py 被直接运行了")
--- main.py
+++ main.py
@@ -4,6 +4,4 @@ print("main.py 执行完成")
 #name = input("请输入你的名字：")
 #print("你好，",name)
 
-print("git版本测试")
 
-print("暂存区测试")
